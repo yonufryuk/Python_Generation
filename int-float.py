25131246b24e033e132f77523cdceb4568af17d3
yonufryuk <93377427+yonufryuk@users.noreply.github.com>
2022-02-23 14:07:56 +0300

    Numeric types int, float. Operator if-else

---
+++ int-float.py
@@ -0,0 +1,67 @@
+# x1, x2 = float(input()), float(input())
+# print(x1 * x2 / 2)
+
+# s1, x1, x2 = float(input()), float(input()), float(input())
+# print(s1 / (x1 + x2))
+
+# x2 = float(input())
+# if x2 == 0:
+#     print('Обратного числа не существует')
+# else:
+#     print(1 / x2)
+
+# x1 = float(input())
+# print(5 / 9 * (x1 - 32))
+
+# x2 = int(input())
+# if x2 == 1:
+#     print(10.5)
+# elif x2 == 2:
+#     print(21)
+# else:
+#     print(21 + (x2 -2) * 4)
+
+# x1 = float(input())
+# print(int(x1 * 10 % 10))
+
+# x1 = float(input())
+# print(x1 - int(x1))
+
+# a = max(3, 8, -3, 12, 9)
+# b = min(3, 8, -3, 12, 9)
+# c = max(3.14, 2.17, 9.8)
+# print(a)
+# print(b)
+# print(c)
+
+# print(abs(10))
+# print(abs(-7))
+# print(abs(0))
+# print(abs(-17.67))
+
+# num = max(1, 3, -5, 7) + min(-3, 6, -8, -1) + abs(-17)
+# print(num)
+
+# x1, x2, x3, x4, x5 = int(input()), int(input()), int(input()), int(input()), int(input())
+# print('Наименьшее число =', min(x1, x2, x3, x4, x5))
+# print('Наибольшее число =', max(x1, x2, x3, x4, x5))
+
+# x1, x2, x3 = int(input()), int(input()), int(input())
+# print(max(x1, x2, x3))
+# if x1 <= x2 <= x3 or x1 >= x2 >= x3:
+#     print(x2)
+# elif x2 <= x1 <= x3 or x2 >= x1 >= x3:
+#     print(x1)
+# else:
+#     print(x3)
+# print(min(x1, x2, x3))
+
+# x = int(input())
+# a = x % 10
+# b = x // 10 % 10
+# c = x // 100
+# if a + b + c == 2 * max(a, b, c):
+#     print("Число интересное")
+# else:
+#     print("Число неинтересное")
+# print(a, b, c)
