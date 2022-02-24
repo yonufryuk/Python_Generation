f32ae0ce7cdfdcf0d9cf5ea454c6212927a8225f
yonufryuk <93377427+yonufryuk@users.noreply.github.com>
2022-02-24 14:58:43 +0300

    math module functions

---
+++ abs+math.py
@@ -0,0 +1,60 @@
+# x1, x2, x3, x4, x5 = float(input()), float(input()), float(input()), float(input()), float(input())
+# print(abs(x1) + abs(x2) + abs(x3) + abs(x4) + abs(x5))
+
+# x1, x2, x3, x4 = int(input()), int(input()), int(input()), int(input())
+# print(abs(x1 - x3) + abs(x2 - x4))
+
+# import math
+# num1 = math.sqrt(2)     # вычисление корня квадратного из двух
+# num2 = math.ceil(3.8)   # округление числа вверх
+# num3 = math.floor(3.8)  # округление числа вниз
+# print(num1)
+# print(num2)
+# print(num3)
+
+# from math import sqrt, pow
+# x1, x2, x3, x4 = float(input()), float(input()), float(input()), float(input())
+# print(sqrt(pow(x1 - x3, 2) + pow(x2 - x4, 2)))
+
+# from math import pi
+# x1 = float(input())
+# print(pi * x1 ** 2)
+# print(pi * x1 * 2)
+
+# import math
+# a, b  = float(input()), float(input())
+# sab, pab = a + b, a * b
+# print(sab / 2)
+# print(math.sqrt(pab))
+# print(2 * pab / sab)
+# print(math.sqrt((a**2 + b**2) / 2))
+
+# from math import *
+# x = radians(float(input()))
+# print(sin(x) + cos(x) + tan(x)**2)
+
+# import math
+# a = float(input())
+# print(math.ceil(a) + math.floor(a))
+
+# from math import *
+# a = float(input())
+# b = float(input())
+# c = float(input())
+# d = b**2-4*a*c
+# if d < 0:
+#     print('Нет корней')
+# elif d == 0:
+#     print(-b / (2*a))
+# elif d > 0:
+#     x1 = (-b - d ** 0.5) / (2*a)
+#     x2 = (-b + d ** 0.5) / (2*a)
+#     print(min(x1, x2))
+#     print(max(x1, x2))
+
+# from math import *
+# n, a = float(input()), float(input())
+# ans = (n * pow(a, 2)) / (4 * tan(pi / n))
+# print(ans)
+
+
