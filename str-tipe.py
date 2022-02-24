fb268e3f070a4b02da73a2fa64bbf2638246011b
yonufryuk <93377427+yonufryuk@users.noreply.github.com>
2022-02-24 15:01:45 +0300

    string data type

---
+++ str-tipe.py
@@ -0,0 +1,36 @@
+# a = input()
+# b = input()
+# print(f"Hello {a} {b}! You just delved into Python")
+
+# print('"Python is a great language!", said Fred. "I don\'t ever remember having this much fun before."')
+
+# c = input()
+# print(f"Футбольная команда {c} имеет длину", len(c), "символов")
+
+# a, b, c = str(input()), str(input()), str(input())
+# print(min(a, b, c, key=len ))
+# print(max(a, b, c, key=len ))
+
+
+# a, b, c = len(input()), len(input()), len(input())
+# if a + b + c == (min(a, b, c) + max(a, b, c))/2*3:
+#     print("YES")
+# else:
+#     print("NO")
+
+# print('YES' if 'синий' in input() else 'NO')
+
+# s = input()
+# print("YES" if "суббота" in s or "воскресенье" in s else "NO")
+
+# s = input()
+# x = y = 0
+# if '.' in s:
+#     x = 1
+# if '@' in s:
+#     y = 1
+# if x + y == 2:
+#     print('YES')
+# else:
+#     print('NO')
+
