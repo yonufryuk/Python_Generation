335d54a9faffbb0d17041425115b8897258cd465
yonufryuk <93377427+yonufryuk@users.noreply.github.com>
2022-02-25 16:05:23 +0300

    cycle for python first steps

---
+++ cirlle-for.py
@@ -0,0 +1,69 @@
+# for i in range(10):
+#   print('Привет')
+
+# text = input()
+# i = int(input())
+# for j in range(i):
+#   print(text)
+
+# for i in range(6):
+#     print('AAA')
+# for i in range(5):
+#     print('BBBB')
+# print('E')
+# for i in range(9):
+#     print('TTTTT')
+# print('G')
+
+# i = int(input())
+# for j in range(i):
+#     print('*******************')
+
+# text = input()
+# for i in range(10):
+#     print(i, text)
+
+# tt = int(input())
+# for i in range(tt + 1):
+#     print('Квадрат числа', i, 'равен', i * i)
+
+# tt = int(input())
+# for i in range(tt):
+#     print('*' * (tt - i))
+
+# aa, bb, cc = int(input()), int(input()), int(input())
+# print('1', float(aa))
+# for i in range(cc - 1):
+#     print(i + 2, aa * bb / 100 + aa)
+#     dd = aa * bb / 100 + aa
+#     aa = dd
+
+# for i in range(5, 0, -1):
+#     print(i, end=' ')
+# print('Взлетаем!!!')
+
+# m, n = int(input()), int(input())
+# for i in range(m, n + 1):
+#     print(i)
+
+# m, n = int(input()), int(input())
+# if m < n:
+#     for i in range(m, n + 1):
+#         print(i)
+# else:
+#     for i in range(m, n - 1, -1):
+#         print(i)
+
+# m, n = int(input()), int(input())
+# for i in range(m, n - 1, -1):
+#     if i % 2 == 1:
+#         print(i)
+
+# m, n = int(input()), int(input())
+# for i in range(m, n + 1):
+#     if i % 17 == 0 or i % 10 == 9 or i % 15 == 0:
+#         print(i)
+
+# m = int(input())
+# for i in range(1, 11):
+#     print(m, 'x', i, '=', m * i)
